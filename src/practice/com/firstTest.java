2035fa3f0d84ac5debea0eaf35602d695fc904f7
Jeyhun <jeyhun_aliyev@partech.com>
2022-02-13 14:18:57 -0500

    modification done

--- src/practice/com/firstTest.java
+++ src/practice/com/firstTest.java
@@ -7,10 +7,11 @@ public class firstTest {
 
 	public static void main(String[] args) {
 	
+		//this method is adding 2 day to current day
 
 	      Calendar cal = Calendar.getInstance();
 		  cal.add(Calendar.DATE, 2); 
-		 String date =  new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime());
+		  String date =  new SimpleDateFormat("yyyy-MM-dd").format(cal.getTime());
 		  System.out.println(date);
 		  
 	}
